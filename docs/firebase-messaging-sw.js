ad1b4d07f3675ac262de0fd81167c35b55671d1a
Mattia <tognoli.mt@gmail.com>
2026-03-07 00:07:30 +0100

    refresh docs web build

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,12 +1,2 @@
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
-
-firebase.initializeApp({
-  apiKey: "dummy",
-  authDomain: "dummy",
-  projectId: "dummy",
-  messagingSenderId: "dummy",
-  appId: "dummy"
-});
-
-const messaging = firebase.messaging();
+// Push notifications are intentionally disabled for now.
+// This service worker is kept as a placeholder for future re-enablement.
