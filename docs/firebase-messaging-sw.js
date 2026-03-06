62b2e192ca88f4adae8cf483b2d24a14fa482720
Mattia <tognoli.mt@gmail.com>
2026-03-06 23:54:14 +0100

    Add firebase messaging service worker

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,24 +1,12 @@
-/* eslint-disable no-undef */
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyAWzGoDeQq2ggURZej6z8kEybDRI2z9z0M',
-  appId: '1:302480518812:web:b7a1d23d05d601eb70c759',
-  messagingSenderId: '302480518812',
-  projectId: 'appkronos-1d181',
-  authDomain: 'appkronos-1d181.firebaseapp.com',
-  storageBucket: 'appkronos-1d181.firebasestorage.app',
+  apiKey: "dummy",
+  authDomain: "dummy",
+  projectId: "dummy",
+  messagingSenderId: "dummy",
+  appId: "dummy"
 });
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  const notification = payload.notification || {};
-  const title = notification.title || 'Nuova notifica';
-  const options = {
-    body: notification.body || '',
-    icon: '/icons/Icon-192.png',
-  };
-  self.registration.showNotification(title, options);
-});
