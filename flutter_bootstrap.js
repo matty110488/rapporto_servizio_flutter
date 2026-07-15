62b489a9b703f1f27afdecfa8cec7d08e25b19b4
matty110488 <matty110488@users.noreply.github.com>
2026-07-15 21:21:03 +0000

    deploy: be115fcea2c29a4a171ce44294224deb3f41f738

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3196311088"
+    serviceWorkerVersion: "1059103810"
   }
 });
